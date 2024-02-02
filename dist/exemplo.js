08f27f016a1e1c4c831a4b28ee46483d34edf496
Diego <contato@laempanadas.com.br>
2024-02-01 21:09:55 -0300

    Remover package-lock.json

--- dist/exemplo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// exemplo.ts
-function dizerOla(nome) {
-    console.log(`Olá, ${nome}!`);
-}
-const meuNome = "Seu Nome";
-dizerOla(meuNome);
--- exemplo.ts
+++
@@ -1,8 +0,0 @@
-// exemplo.ts
-function dizerOla(nome: string) {
-    console.log(`Olá, ${nome}!`);
-  }
-  
-  const meuNome: string = "Seu Nome";
-  dizerOla(meuNome);
-  
