cc5aa3ac038832e57ee402b5a76dff91d5cf56ea
Diego <contato@laempanadas.com.br>
2024-04-03 23:05:13 -0300

    .env

--- inc/db.js
+++ inc/db.js
@@ -1,11 +1,13 @@
-const mysql = require ('mysql2');
+require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
+
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'user',
-    database: 'empanadas',
-    password:'Bucetinha19#',
-    multipleStatements: true
-  });
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  multipleStatements: true
+});
 
-  module.exports = connection;
+module.exports = connection;
