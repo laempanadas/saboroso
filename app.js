d8380364d84cb006a564dbd946250a8313c5c91d
Diego <contato@laempanadas.com.br>
2024-06-20 19:26:03 -0300

    mysql2

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.set('view engine', 'ejs');
 // Configuração da sessão
 app.use(session({
   store: new redisStore({
-    host: '127.0.0.1',
+    host: 'localhost',
     port: 6379
   }),
   secret: 'sh@llom',
